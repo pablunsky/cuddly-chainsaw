638e59c594bfe3125dfa24e267bed547e3c70ff7
pablunsky <hpablo677@gmail.com>
2018-10-11 00:15:29 -0600

    Creacion de servidor, primer app

---
+++ Servidor/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/pablunsky/Documents/TAREAS/ARCHIVOS/Proyecto 2/Servidor/Servidor/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
